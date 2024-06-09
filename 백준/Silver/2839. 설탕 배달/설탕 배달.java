1d3f96fec063cab81a089f2d39644bdc099f29db
jini <163384235+jinijavac@users.noreply.github.com>
2024-06-09 22:59:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int bags = 0;
+
+        if (n == 4 || n == 7){
+            //1, 2, 4, 7은 3이나 5로 나누어지지 않음
+            System.out.println(-1);
+
+        }else if (n % 5 == 0){
+            System.out.println(n/5);
+
+        }else if (n % 5 == 1 || n % 5 == 3){
+            // 나머지가 1인 경우: n = 5a + 6 (3kg * 2)
+            // ex) 6 -> 3kg 2개
+            // 나머지가 3인 경우: n = 5a + 3 (5kg * 1 + 3kg * 1)
+            // ex) 8 -> 5kg 1개 3kg 1개
+            System.out.println(n/5 +1);
+
+        }else if (n % 5 == 2 || n % 5 == 4){
+            // n % 5 == 2인 경우: n = 5a + 12 (3kg * 4)
+            //ex) 12 -> 3+3+3+3
+            // n % 5 == 4인 경우: n = 5a + 9 (3kg * 3)
+            //ex) 9 -> 3+3+3
+            System.out.println(n/5 +2);
+        }
+    }
+}
